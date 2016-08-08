7ad8f79f1daacf9db3c2673c009353bcb40bb3e7
Vladimir Makarevich <business.makarevich@outlook.com>
2016-08-08 13:52:15 +0300

    Work with widget slider. Adding comment for post. Rebuild DB model.

---
+++ Rhythm/Controllers/CommentsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rhythm.Controllers
+{
+    public class CommentsController : Controller
+    {
+        // GET: Comments
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult RecentComments()
+        {
+            var comments = GetRecentComments();
+            return PartialView("RecentComments", comments);
+        }
+
+        private object GetRecentComments()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Rhythm/Models/CommentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Rhythm.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rhythm.Models
+{
+    public class CommentViewModel
+    {
+        public Post Post { get; set; }
+        public Comment Comment { get; set; }
+        public string DisplayName { get; set; }
+        public string IsHuman { get; set; }
+    }
+}
