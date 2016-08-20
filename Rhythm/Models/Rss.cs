f2331465f221265d4d19a53d54b6cabbdd89842a
VladiMakarevich <justadreampictures@gmail.com>
2016-08-20 13:39:13 +0300

    add RSS dev.by

---
+++ Rhythm/Models/Rss.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text.RegularExpressions;
+
+namespace Rhythm.Models
+{
+    public class Rss
+    {
+        public string Link { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string PubDate { get; set; }
+
+    }
+}
---
+++ Rhythm/Models/RssListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rhythm.Models
+{
+    public class RssListViewModel
+    {
+        public RssReader rssReader { get; set; }
+        public ListView PagingView { get; set; }
+    }
+}
