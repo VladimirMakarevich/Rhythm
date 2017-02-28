c721064b304b3c0e7eefcf0ab40eec85e40354ff
Vladimir Makarevich <business.makarevich@outlook.com>
2017-02-28 12:42:55 +0300

    remove unusage namespaces

--- Rhythm/Models/CommentViewModel.cs
+++ Rhythm/Models/CommentViewModel.cs
@@ -1,10 +1,4 @@
-﻿using Rhythm.Domain.Model;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Rhythm.Models
 {
--- Rhythm/Models/PostListViewModel.cs
+++ Rhythm/Models/PostListViewModel.cs
@@ -1,8 +1,5 @@
 ﻿using Rhythm.Domain.Model;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace Rhythm.Models
 {
