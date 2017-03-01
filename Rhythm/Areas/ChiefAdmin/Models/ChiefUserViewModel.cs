491148e8b392ed077c5b036d06a9f8fd53ef7f68
Vladimir Makarevich <business.makarevich@outlook.com>
2017-03-01 23:23:00 +0300

    create ViewModel for portfolio and User

--- Rhythm/Areas/ChiefAdmin/Models/ChiefUserViewModel.cs
+++ Rhythm/Areas/ChiefAdmin/Models/ChiefUserViewModel.cs
@@ -7,5 +7,17 @@ namespace Rhythm.Areas.ChiefAdmin.Models
 {
     public class ChiefUserViewModel
     {
+        public int ChiefUserID { get; set; }
+        public int PortfolioID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Birth { get; set; }
+        public string Email { get; set; }
+        public string HomeAddress { get; set; }
+        public string Skype { get; set; }
+        public string Mobile { get; set; }
+        public string Github { get; set; }
+        public string Linkedin { get; set; }
     }
 }
--- Rhythm/Areas/ChiefAdmin/Models/PortfolioViewModel.cs
+++ Rhythm/Areas/ChiefAdmin/Models/PortfolioViewModel.cs
@@ -7,5 +7,13 @@ namespace Rhythm.Areas.ChiefAdmin.Models
 {
     public class PortfolioViewModel
     {
+        public int PortfolioID { get; set; }
+        public string Summary { get; set; }
+        public string Skills { get; set; }
+        public string WorkExp { get; set; }
+        public string MyProjects { get; set; }
+        public string Education { get; set; }
+        public string AdditionalInfo { get; set; }
+        //public virtual ICollection<int> ChiefUsers { get; set; }
     }
 }
