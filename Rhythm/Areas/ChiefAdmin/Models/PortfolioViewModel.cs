bcc55def6afb0557a174417f01e629765befcf89
VladiMakarevich <justadreampictures@gmail.com>
2017-03-01 14:38:36 +0300

    add view model in admn area for mappings

---
+++ Rhythm/Areas/ChiefAdmin/Models/PortfolioViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rhythm.Areas.ChiefAdmin.Models
+{
+    public class PortfolioViewModel
+    {
+    }
+}
