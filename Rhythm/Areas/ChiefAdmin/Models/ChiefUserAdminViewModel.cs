b138830c83bfc0cf1be2c6f30e319ffac9240e34
VladimirMakarevich <makarevich.dev@outlook.com>
2017-06-29 10:47:51 +0300

    added content and refactoring aboutme controllers with UI

--- Rhythm/Areas/ChiefAdmin/Models/ChiefUserAdminViewModel.cs
+++ Rhythm/Areas/ChiefAdmin/Models/ChiefUserAdminViewModel.cs
@@ -1,20 +1,32 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Rhythm.Areas.ChiefAdmin.Models
 {
     public class ChiefUserAdminViewModel
     {
         public int Id { get; set; }
+        [Display(Name="Portfolio")]
         public int PortfolioId { get; set; }
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Display(Name = "Middle Name")]
         public string MiddleName { get; set; }
+        [Display(Name = "Birth")]
         public string Birth { get; set; }
+        [Display(Name = "Email")]
         public string Email { get; set; }
+        [Display(Name = "Address")]
         public string HomeAddress { get; set; }
+        [Display(Name = "Skype")]
         public string Skype { get; set; }
+        [Display(Name = "Mobile")]
         public string Mobile { get; set; }
+        [Display(Name = "GitHub")]
         public string Github { get; set; }
+        [Display(Name = "LinkedIn")]
         public string Linkedin { get; set; }
 
         public List<PortfolioAdminViewModel> PortfolioViewModel { get; set; }
--- Rhythm/Areas/ChiefAdmin/Models/PortfolioAdminViewModel.cs
+++ Rhythm/Areas/ChiefAdmin/Models/PortfolioAdminViewModel.cs
@@ -1,16 +1,23 @@
-﻿namespace Rhythm.Areas.ChiefAdmin.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rhythm.Areas.ChiefAdmin.Models
 {
     public class PortfolioAdminViewModel
     {
         public int Id { get; set; }
+        [Display(Name = "Portfolio")]
         public string NamePortfolio { get; set; }
         public string Objective { get; set; }
         public string Summary { get; set; }
         public string Skills { get; set; }
+        [Display(Name = "English Proficiency")]
         public string EnglishProficiency { get; set; }
+        [Display(Name = "Work Experience")]
         public string WorkExp { get; set; }
         public string Education { get; set; }
+        [Display(Name = "Application Systems")]
         public string ApplicationSystems { get; set; }
+        [Display(Name = "Additional Information")]
         public string AdditionalInfo { get; set; }
         public int ChiefUserId { get; set; }
         public ChiefUserAdminViewModel ChiefUser { get; set; }
